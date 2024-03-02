70cf8e6015ab3b30301d99fe067d083945111a54
octuaOSdev <144250548+octuaOSdev@users.noreply.github.com>
2024-03-02 22:27:36 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,7 +14,17 @@ if [ ! -f "$FILE" ]; then
     cd server
     touch true.txt
     touch eula.txt
+    result=$(dialog --title "EULA" --inputbox "Do you accept minecrafts EULA?:" 10 40 2>&1 >/dev/tty)
+    # Example: Check if the user's response is "yes" (case-insensitive)
+if [[ "${result,,}" == "yes" ]]; then
     echo "eula=true" > /home/"$username"/server/eula.txt
+else
+    bash uninstall.sh
+    exit 400
+fi
+
+    
+    
     wget  -P /home/"$username"/server/ "https://piston-data.mojang.com/v1/objects/8dd1a28015f51b1803213892b50b7b4fc76e594d/server.jar"
     cd ..
     cd Desktop
